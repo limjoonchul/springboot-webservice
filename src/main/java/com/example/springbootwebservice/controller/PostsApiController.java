fd14ac6f0a26b27c8eeeb76ef7cd84ec5b0bd642
limjoonchul <dlawnscjf511@naver.com>
2021-05-04 12:49:36 +0900

    PostsApiController 생성

---
+++ src/main/java/com/example/springbootwebservice/controller/PostsApiController.java
@@ -0,0 +1,23 @@
+package com.example.springbootwebservice.controller;
+
+import com.example.springbootwebservice.dto.PostsSaveRequestDto;
+import com.example.springbootwebservice.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+
+    }
+}
