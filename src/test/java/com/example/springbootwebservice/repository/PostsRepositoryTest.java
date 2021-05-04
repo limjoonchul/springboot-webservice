16d77e03faa5739383322797c07fd5b403011948
limjoonchul <dlawnscjf511@naver.com>
2021-05-04 13:28:28 +0900

    BaseTimeEntity_등록() 테스트 추가

--- src/test/java/com/example/springbootwebservice/repository/PostsRepositoryTest.java
+++ src/test/java/com/example/springbootwebservice/repository/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -39,6 +40,31 @@ class PostsRepositoryTest {
 
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2020,03,17,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate="+posts.getCreateDate()
+                +", modeifeidDate="+posts.getModifiedDate());
+
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
 
     }
 }
