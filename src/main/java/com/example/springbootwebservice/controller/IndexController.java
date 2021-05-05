92bf3dc7bfe66f52780e20d383d9dc5817d30e4e
limjoonchul <dlawnscjf511@naver.com>
2021-05-05 10:42:40 +0900

    postsSave() 추가

--- src/main/java/com/example/springbootwebservice/controller/IndexController.java
+++ src/main/java/com/example/springbootwebservice/controller/IndexController.java
@@ -12,4 +12,9 @@ public class IndexController {
         // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정 된다.
         // index.mustache로 가게 된다.
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
 }
