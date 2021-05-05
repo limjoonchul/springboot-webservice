5e562e8dd34bd2773ad5e9d9c55fbb930968b434
limjoonchul <dlawnscjf511@naver.com>
2021-05-05 10:36:23 +0900

    indexController 생성

---
+++ src/main/java/com/example/springbootwebservice/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.springbootwebservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+        // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정 된다.
+        // index.mustache로 가게 된다.
+    }
+}
