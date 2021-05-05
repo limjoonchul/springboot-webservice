970360f40b9641e638078f229d5c28438e8590f9
limjoonchul <dlawnscjf511@naver.com>
2021-05-05 11:24:58 +0900

    postsUpdate 메소드 추가

--- src/main/java/com/example/springbootwebservice/controller/IndexController.java
+++ src/main/java/com/example/springbootwebservice/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.example.springbootwebservice.controller;
 
 import com.example.springbootwebservice.domain.Posts;
+import com.example.springbootwebservice.dto.PostsResponseDto;
 import com.example.springbootwebservice.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,5 +27,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
