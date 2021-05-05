0b5f7d23a40978af7ebd66ae81ad8aa28656f48d
limjoonchul <dlawnscjf511@naver.com>
2021-05-05 15:38:48 +0900

    BaseTimeEntity 상속 코드 추가

--- src/main/java/com/example/springbootwebservice/domain/User.java
+++ src/main/java/com/example/springbootwebservice/domain/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class User {
+public class User extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
