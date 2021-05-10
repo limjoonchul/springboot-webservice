68e14ffcf336219d8756d0cce98a250eede039c8
limjoonchul <dlawnscjf511@naver.com>
2021-05-10 14:44:20 +0900

    index 메소드 오타 수정

--- src/main/java/com/example/springbootwebservice/controller/IndexController.java
+++ src/main/java/com/example/springbootwebservice/controller/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
 
         if(user != null){
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("username", user.getName());
         }
 
         return "index";
