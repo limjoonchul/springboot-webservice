ed1c94cf65c75f8dc5dc927eec7cc37b843428ac
limjoonchul <dlawnscjf511@naver.com>
2021-05-12 17:27:15 +0900

    start.sh 코드 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
@@ -5,17 +6,17 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot-webservice
+PROJECT_NAME=demo-springboot-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
